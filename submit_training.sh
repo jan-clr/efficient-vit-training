bf4a4acb46d00c43f0962b0687eeb06c60c1c870
Jan Claar <mail@jan-claar.de>
2025-02-04 19:35:51 +0100

    feat: make work on cluster

---
+++ submit_training.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l
+
+cd $HOME/repos/efficient-vit-training/
+
+mkdir -p logs
+
+timestamp=$(date +%Y%m%d%H%M%S)
+
+runname="vit_training_${timestamp}"
+
+sbatch.tinygpu --job-name=$runname --output=logs/$runname.log --mail-user='mail@jan-claar.de' --mail-type=ALL varsize_1gpu_best.sh
--- varsize_1gpu_best.sh
+++ varsize_1gpu_best.sh
@@ -1,22 +1,15 @@
-# Copyright (c) 2023-present, Royal Bank of Canada.
-# All rights reserved.
-#
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-#
 #!/bin/bash
-
-#SBATCH --nodelist= <node list>
-#SBATCH --mem=40G
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:a100:1
 #SBATCH --ntasks=1
-#SBATCH --output= <output file name>
+#SBATCH --partition=a100
+#SBATCH --time=24:00:00
 
 hostname
 whoami
 echo $CUDA_VISIBLE_DEVICES
-source .bashrc # or .zsh or whatever source file you use
-cd <directory of code>
-conda activate <name of environment>
-python3 -m torch.distributed.launch --nproc_per_node=1 --master_port 53533 --use_env main.py --model deit_small_patch16_224 --batch-size 64 --data-path <path to imagenet> --output_dir <output directory> --lr 1e-3 --localvit --localvit-act 'hs' --is-multisize --init-size 32 --epoch-step 5 --patch-step 2 --eval-on-final-size
+
+module load cuda/11.8.0
+. .venv/bin/activate
+
+python3 -m torch.distributed.launch --nproc_per_node=1 --master_port 53533 --use_env main.py --model deit_small_patch16_224 --batch-size 64 --data-path $WORK/datasets/imagenet/ --output_dir $WORK/vit_runs --lr 1e-3 --localvit --localvit-act 'hs' --is-multisize --init-size 32 --epoch-step 5 --patch-step 2 --eval-on-final-size
 echo "DONE"
