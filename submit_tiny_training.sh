455868fcff1ce2185473653e940992edac4a5e13
jc <mail@jan-claar.de>
2025-07-16 11:20:49 +0200

    Update efficient vit repo

--- submit_tiny_training.sh
+++ submit_tiny_training.sh
@@ -8,4 +8,4 @@ timestamp=$(date +%Y%m%d%H%M%S)
 
 runname="vit_training_${timestamp}"
 
-sbatch.tinygpu --job-name=$runname --output=logs/$runname.log --mail-user='mail@jan-claar.de' --mail-type=ALL varsize_1gpu_best_tinyimnet.sh
+sbatch --job-name=$runname --output=logs/$runname.log --mail-user='mail@jan-claar.de' --mail-type=ALL varsize_1gpu_best_tinyimnet.sh
