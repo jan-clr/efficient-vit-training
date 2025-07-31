2d70071665483bab8e8feccc6dd1c773e288ef85
jc <mail@jan-claar.de>
2025-07-31 13:37:23 +0200

    cleanup round 4

--- submit_cifar_training.sh
+++ submit_cifar_training.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -l
 
-cd $HOME/repos/efficient-vit-training/
 
 mkdir -p logs
 
@@ -8,4 +7,4 @@ timestamp=$(date +%Y%m%d%H%M%S)
 
 runname="vit_training_${timestamp}"
 
-sbatch --job-name=$runname --output=logs/$runname.log --mail-user='mail@jan-claar.de' --mail-type=ALL varsize_1gpu_best_cifar.sh
+sbatch --job-name=$runname --output=logs/$runname.log --mail-user='<YOUR-MAIL-ADDRESS' --mail-type=ALL varsize_1gpu_best_cifar.sh
